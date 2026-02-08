974cfee481cf66375b1e5380ce1169831a638999
Capsule Drops <hello@capsuledrops.com>
2026-02-08 22:16:52 +0000

    Add node-fetch dependency for Gelato API calls

--- netlify/functions/stripe-webhook.js
+++ netlify/functions/stripe-webhook.js
@@ -1,4 +1,5 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const fetch = require('node-fetch');
 
 // Gelato API configuration
 const GELATO_API_KEY = process.env.GELATO_API_KEY;
